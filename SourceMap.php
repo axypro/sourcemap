49202c54ca03099ed9ef84e90ecb3951d9f312bc
Oleg Grigoriev <go.vasac@gmail.com>
2015-02-28 18:03:25 +0300

    SourceMap::$version: the version of the file format

--- SourceMap.php
+++ SourceMap.php
@@ -6,11 +6,17 @@
 
 namespace axy\sourcemap;
 
+use axy\errors\ContainerReadOnly;
 use axy\sourcemap\parsing\FormatChecker;
 use axy\sourcemap\parsing\Context;
+use axy\sourcemap\errors\UnsupportedVersion;
+use axy\errors\FieldNotExist;
 
 /**
  * The Source Map Class
+ *
+ * @property-read int $version
+ *                the version of the file format
  */
 class SourceMap
 {
@@ -33,6 +39,71 @@ class SourceMap
     public function getData()
     {
         return $this->context->getOutData();
+    }
+
+    /**
+     * Magic isset()
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return array_key_exists($key, $this->context->data);
+    }
+
+    /**
+     * Magic get
+     *
+     * @param string $key
+     * @return mixed
+     * @throws \axy\errors\FieldNotExist
+     */
+    public function __get($key)
+    {
+        switch ($key) {
+            case 'version':
+                return 3;
+        }
+        throw new FieldNotExist($key, $this, null, $this);
+    }
+
+    /**
+     * Magic set
+     *
+     * @param string $key
+     * @param mixed $value
+     * @throws \axy\errors\FieldNotExist
+     */
+    public function __set($key, $value)
+    {
+        switch ($key) {
+            case 'version':
+                if (($value === 3) || ($value === '3')) {
+                    return;
+                }
+                throw new UnsupportedVersion($value);
+        }
+        throw new FieldNotExist($key, $this, null, $this);
+    }
+
+    /**
+     * Magic unset
+     *
+     * @param string $key
+     * @throws \axy\errors\ContainerReadOnly
+     */
+    public function __unset($key)
+    {
+        throw new ContainerReadOnly($this, null, $this);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return '[Source Map]';
     }
 
     /**
--- tests/SourceMapTest.php
+++ tests/SourceMapTest.php
@@ -26,4 +26,52 @@ class SourceMapTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('axy\sourcemap\errors\InvalidFormat');
         return new SourceMap(['version' => 5]);
     }
+
+    public function testPropertyVersion()
+    {
+        $map = new SourceMap();
+        $this->assertSame(3, $map->version);
+        $this->assertTrue(isset($map->version));
+        $map->version = 3;
+        $map->version = '3';
+        $this->assertSame(3, $map->version);
+        $this->setExpectedException('axy\sourcemap\errors\UnsupportedVersion');
+        $map->version = 5;
+    }
+
+    /**
+     * covers ::__get
+     * @expectedException \axy\errors\FieldNotExist
+     */
+    public function testMagicGet()
+    {
+        $map = new SourceMap();
+        return $map->abc;
+    }
+
+    /**
+     * covers ::__set
+     * @expectedException \axy\errors\FieldNotExist
+     */
+    public function testMagicSet()
+    {
+        $map = new SourceMap();
+        $map->abc = 1;
+    }
+
+    /**
+     * covers ::__unset
+     * @expectedException \axy\errors\ContainerReadOnly
+     */
+    public function testMagicUnset()
+    {
+        $map = new SourceMap();
+        unset($map->version);
+    }
+
+    public function testMagicIsset()
+    {
+        $map = new SourceMap();
+        $this->assertFalse(isset($map->abc));
+    }
 }
