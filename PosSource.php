ae304d89dd2fc7d29988a74d8b8c189c156ca867
Oleg Grigoriev <go.vasac@gmail.com>
2015-02-28 16:46:59 +0300

    Position in the source

---
+++ PosSource.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package axy\sourcemap
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+namespace ayx\sourcemap;
+
+/**
+ * Position in the source
+ */
+class PosSource
+{
+    /**
+     * A source file as an index in the "sources" section
+     *
+     * @var int
+     */
+    public $fileIndex;
+
+    /**
+     * A source file name
+     *
+     * @var string
+     */
+    public $fileName;
+
+    /**
+     * A line number (zero-based)
+     *
+     * @var int
+     */
+    public $line;
+
+    /**
+     * A column number (zero-based)
+     *
+     * @var int
+     */
+    public $column;
+
+    /**
+     * A symbol name as an index in the "names" section
+     *
+     * @var int
+     */
+    public $nameIndex;
+
+    /**
+     * A symbol name
+     *
+     * @var string
+     */
+    public $name;
+}
