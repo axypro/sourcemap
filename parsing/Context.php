57dae5ef96d42f04a5f78d1b364771ec7cade2af
Oleg Grigoriev <go.vasac@gmail.com>
2015-02-28 17:34:57 +0300

    parsing\Contents::getOutData(): Returns the data for JSON file

--- parsing/Context.php
+++ parsing/Context.php
@@ -27,4 +27,29 @@ class Context
     {
         $this->data = $data;
     }
+
+    /**
+     * Returns the data for JSON file
+     *
+     * @return array
+     */
+    public function getOutData()
+    {
+        $result = ['version' => 3];
+        $fields = ['file', 'sourceRoot', 'sources', 'sourcesContent', 'names', 'mappings'];
+        foreach ($fields as $field) {
+            if (isset($this->data[$field])) {
+                $value = $this->data[$field];
+                if (is_array($value)) {
+                    if (!empty($value)) {
+                        $result[$field] = $value;
+                    }
+                } elseif (($value !== '') && ($value !== null)) {
+                    $result[$field] = $value;
+                }
+            }
+        }
+
+        return $result;
+    }
 }
--- tests/parsing/ContextTest.php
+++ tests/parsing/ContextTest.php
@@ -28,4 +28,61 @@ class ContextTest extends \PHPUnit_Framework_TestCase
         $context = new Context($data);
         $this->assertEquals($data, $context->data);
     }
+
+    /**
+     * covers ::getOutData
+     * @dataProvider providerGetOutData
+     * @param array $input
+     * @param array $output
+     */
+    public function testGetOutData(array $input, array $output)
+    {
+        $context = new Context($input);
+        $this->assertEquals($output, $context->getOutData());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerGetOutData()
+    {
+        return [
+            [
+                [
+                    'version' => 3,
+                    'file' => 'out.js',
+                    'sourceRoot' => '/js/',
+                    'sources' => ['a.js', 'b.js'],
+                    'sourcesContent' => [null, 'B contents'],
+                    'names' => ['one', 'two'],
+                    'mappings' => 'AAAAA;ACAC',
+                ],
+                [
+                    'version' => 3,
+                    'file' => 'out.js',
+                    'sourceRoot' => '/js/',
+                    'sources' => ['a.js', 'b.js'],
+                    'sourcesContent' => [null, 'B contents'],
+                    'names' => ['one', 'two'],
+                    'mappings' => 'AAAAA;ACAC',
+                ],
+            ],
+            [
+                [
+                    'version' => 3,
+                    'file' => 'out.js',
+                    'sourceRoot' => '',
+                    'sources' => ['a.js', 'b.js'],
+                    'names' => [],
+                    'mappings' => 'AAAAA;ACAC',
+                ],
+                [
+                    'version' => 3,
+                    'file' => 'out.js',
+                    'sources' => ['a.js', 'b.js'],
+                    'mappings' => 'AAAAA;ACAC',
+                ],
+            ],
+        ];
+    }
 }
