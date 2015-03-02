9b3539e910151344d66740b8e1c0e36594c4b823
Oleg Grigoriev <go.vasac@gmail.com>
2015-03-02 14:33:33 +0300

    Refactoring SourceMap test

--- tests/classSourceMap/MagicTest.php
+++ tests/classSourceMap/MagicTest.php
@@ -4,59 +4,15 @@
  * @author Oleg Grigoriev <go.vasac@gmail.com>
  */
 
-namespace axy\sourcemap\tests;
+namespace axy\sourcemap\tests\classSourceMap;
 
 use axy\sourcemap\SourceMap;
 
 /**
  * coversDefaultClass axy\sourcemap\SourceMap
  */
-class SourceMapTest extends \PHPUnit_Framework_TestCase
+class MagicTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * covers ::__construct
-     * covers ::getData
-     * @return \axy\sourcemap\SourceMap
-     */
-    public function testCreate()
-    {
-        $map = new SourceMap();
-        $expected = [
-            'version' => 3,
-            'file' => '',
-            'sourceRoot' => '',
-            'sources' => [],
-            'names' => [],
-            'mappings' => '',
-        ];
-        $this->assertEquals($expected, $map->getData());
-        $this->setExpectedException('axy\sourcemap\errors\InvalidFormat');
-        return new SourceMap(['version' => 5]);
-    }
-
-    /**
-     * covers ::getData
-     */
-    public function testGetData()
-    {
-        $data = [
-            'version' => 3,
-            'file' => 'out.js',
-            'sources' => ['a.js'],
-            'mappings' => 'A;C',
-        ];
-        $expected = [
-            'version' => 3,
-            'file' => 'out.js',
-            'sourceRoot' => '',
-            'sources' => ['a.js'],
-            'names' => [],
-            'mappings' => 'A;C',
-        ];
-        $map = new SourceMap($data);
-        $this->assertSame($expected, $map->getData());
-    }
-
     public function testPropertyVersion()
     {
         $map = new SourceMap();
