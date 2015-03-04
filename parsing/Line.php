5c0dfc30e603bca27e077bfa10a13e63f30edb83
Oleg Grigoriev <go.vasac@gmail.com>
2015-03-04 20:06:13 +0300

    parsing\Line::loadFromMappings(): Loads the positions list from a mappings line

--- parsing/Line.php
+++ parsing/Line.php
@@ -6,6 +6,8 @@
 
 namespace axy\sourcemap\parsing;
 
+use axy\sourcemap\errors\InvalidMappings;
+
 /**
  * A line in the generated content
  */
@@ -42,6 +44,48 @@ class Line
     }
 
     /**
+     * Loads the positions list from a mappings line
+     *
+     * @param int $num
+     * @param string $lMappings
+     * @param \axy\sourcemap\parsing\SegmentParser $parser
+     * @param \axy\sourcemap\parsing\Context $context
+     * @return \axy\sourcemap\parsing\Line
+     * @throws \axy\sourcemap\errors\InvalidMappings
+     */
+    public static function loadFromMappings($num, $lMappings, SegmentParser $parser, Context $context)
+    {
+        $positions = [];
+        $names = $context->names;
+        $files = $context->sources;
+        $parser->nextLine($num);
+        foreach (explode(',', $lMappings) as $segment) {
+            $pos = $parser->parse($segment);
+            $positions[$pos->generated->column] = $pos;
+            $source = $pos->source;
+            $fi = $source->fileIndex;
+            if ($fi !== null) {
+                if (isset($files[$fi])) {
+                    $source->fileName = $files[$fi];
+                } else {
+                    $message = 'Invalid segment "'.$segment.'" (source offset '.$fi.')';
+                    throw new InvalidMappings($message);
+                }
+                $ni = $source->nameIndex;
+                if ($ni !== null) {
+                    if (isset($names[$ni])) {
+                        $source->name = $names[$ni];
+                    } else {
+                        $message = 'Invalid segment "'.$segment.'" (name offset '.$ni.')';
+                        throw new InvalidMappings($message);
+                    }
+                }
+            }
+        }
+        return new self($num, $positions);
+    }
+
+    /**
      * Returns the line number
      *
      * @return int
@@ -59,6 +103,21 @@ class Line
     public function getPositions()
     {
         return $this->positions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTestPositions()
+    {
+        $result = [];
+        foreach ($this->positions as $k => $pos) {
+            $result[$k] = [
+                'generated' => (array)$pos->generated,
+                'source' => (array)$pos->source,
+            ];
+        }
+        return $result;
     }
 
     /**
--- tests/parsing/LineTest.php
+++ tests/parsing/LineTest.php
@@ -7,6 +7,8 @@
 namespace axy\sourcemap\tests\parsing;
 
 use axy\sourcemap\parsing\Line;
+use axy\sourcemap\parsing\SegmentParser;
+use axy\sourcemap\parsing\Context;
 use axy\sourcemap\PosMap;
 
 /**
@@ -49,5 +51,162 @@ class LineTest extends \PHPUnit_Framework_TestCase
         ];
         $this->assertEquals($expected, $line->getPositions());
         $this->assertSame(10, $line->getNum());
+    }
+
+    /**
+     * covers ::loadFromMappings
+     */
+    public function testLoadFromMappings()
+    {
+        $data = [
+            'version' => 3,
+            'sources' => ['a.js', 'b.js'],
+            'names' => ['one', 'two', 'three', 'four', 'five'],
+            'mappings' => 'A',
+        ];
+        $context = new Context($data);
+        $parser = new SegmentParser();
+        $lMappings1 = 'AAAA,YAAY,CAAC';
+        $line1 = Line::loadFromMappings(0, $lMappings1, $parser, $context);
+        $this->assertInstanceOf('axy\sourcemap\parsing\Line', $line1);
+        $expected1 = [
+            0 => [
+                'generated' => [
+                    'line' => 0,
+                    'column' => 0,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 0,
+                    'column' => 0,
+                    'nameIndex' => null,
+                    'name' => null,
+                ],
+            ],
+            12 => [
+                'generated' => [
+                    'line' => 0,
+                    'column' => 12,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 0,
+                    'column' => 12,
+                    'nameIndex' => null,
+                    'name' => null,
+                ],
+            ],
+            13 => [
+                'generated' => [
+                    'line' => 0,
+                    'column' => 13,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 0,
+                    'column' => 13,
+                    'nameIndex' => null,
+                    'name' => null,
+                ],
+            ],
+        ];
+        $this->assertSame($expected1, $line1->getTestPositions());
+        $lMappings2 = 'AAEb,IAAOC,GAAG,WAAWE';
+        $line2 = Line::loadFromMappings(8, $lMappings2, $parser, $context);
+        $this->assertInstanceOf('axy\sourcemap\parsing\Line', $line2);
+        $expected2 = [
+            0 => [
+                'generated' => [
+                    'line' => 8,
+                    'column' => 0,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 2,
+                    'column' => 0,
+                    'nameIndex' => null,
+                    'name' => null,
+                ],
+            ],
+            4 => [
+                'generated' => [
+                    'line' => 8,
+                    'column' => 4,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 2,
+                    'column' => 7,
+                    'nameIndex' => 1,
+                    'name' => 'two',
+                ],
+            ],
+            7 => [
+                'generated' => [
+                    'line' => 8,
+                    'column' => 7,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 2,
+                    'column' => 10,
+                    'nameIndex' => null,
+                    'name' => null,
+                ],
+            ],
+            18 => [
+                'generated' => [
+                    'line' => 8,
+                    'column' => 18,
+                ],
+                'source' => [
+                    'fileIndex' => 0,
+                    'fileName' => 'a.js',
+                    'line' => 2,
+                    'column' => 21,
+                    'nameIndex' => 3,
+                    'name' => 'four',
+                ],
+            ],
+        ];
+        $this->assertSame($expected2, $line2->getTestPositions());
+    }
+
+    /**
+     * covers ::loadFromMappings
+     * @dataProvider providerErrorLoad
+     * @expectedException \axy\sourcemap\errors\InvalidMappings
+     */
+    public function testErrorLoad($lMappings)
+    {
+        $data = [
+            'version' => 3,
+            'sources' => ['a.js', 'b.js'],
+            'names' => ['one', 'two', 'three', 'four', 'five'],
+            'mappings' => 'A',
+        ];
+        $context = new Context($data);
+        $parser = new SegmentParser();
+        Line::loadFromMappings(2, $lMappings, $parser, $context);
+    }
+
+    /**
+     * @return array
+     */
+    public function providerErrorLoad()
+    {
+        return [
+            'empty' => [''],
+            'count3' => ['AAA'],
+            'base64' => ['A*AA'],
+            'vlq' => ['AAAz'],
+            'indexed' => ['AZAA'],
+        ];
     }
 }
