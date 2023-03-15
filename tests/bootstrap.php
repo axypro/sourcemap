318d7ea190b8e672e2d88eff264b7568d01f2889
Oleg Grigoriev <go.vasac@gmail.com>
2023-03-15 22:58:10 +0400

    Fix tests: create local directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 namespace axy\sourcemap\tests;
 
 require_once __DIR__ . '/../index.php';
+
+if (!file_exists(__DIR__ . '/../local')) {
+    mkdir(__DIR__ . '/../local');
+}
